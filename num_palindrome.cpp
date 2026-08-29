996917c7c914cffe04ab2daf76adda7fb75ea844
Anutosh <anutosh.krishanu@s.amity.edu>
2026-08-30 01:34:33 +0530

    Add num_palindrome.cpp

---
+++ num_palindrome.cpp
@@ -0,0 +1,22 @@
+// Palindrome of a number
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, r, sum = 0, temp;
+    cout << "Enter a number: ";
+    cin >> n;
+    temp = n;
+    while (n > 0)
+    {
+        r = n % 10 ;
+        sum = (sum * 10) + r;
+        n=n/10;
+    }
+    if (temp==sum)
+        cout << " number is a palindrome.";
+    else
+        cout << " number is not a palindrome.";
+
+    return 0;
+}
